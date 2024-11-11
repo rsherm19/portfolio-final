0fb55401d2a167a04301c55e8973b583e0c35f74
Ryan Sherman <rsherman5@elon.edu>
2024-11-10 21:38:18 -0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hi, I'm Ryan.</h1>
+    </>
+  )
+}
+
+export default App
