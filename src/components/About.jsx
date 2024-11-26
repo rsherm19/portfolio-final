e9c2b2f402a0ce2de1cbef31f236f03389bccbe5
Ryan Sherman <rsherman5@elon.edu>
2024-11-26 15:33:14 -0500

    import photo to fix issue

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import aboutPhoto from './assets/images/ryan.jpg'
 
 const About = () => {
   return (
     <>
       <h1>About Me:</h1>
       <div className='about-flex-container'>
-        <img src="../assets/images/ryan.jpg" alt="man with blonde hair smiling" className='profile-photo' />
+        <img src={aboutPhoto} alt="man with blonde hair smiling" className='profile-photo' />
         <p className='about-description'>I am a full-stack software engineer who attacks problems with a creative, entrepreneurial mindset. I strive to grow and improve rather than simply get the job done. I have experience creating music as a producer and mixing engineer, and I continually carry that creativity and critical thinking into my full-stack software development.</p>
       </div>
     </>
