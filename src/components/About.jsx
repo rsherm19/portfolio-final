471b48a222eeb3f3fcaf02e1dc71a0c26462d927
Ryan Sherman <rsherman5@elon.edu>
2024-11-26 01:00:40 -0500

    fix routing for pdf

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <>
       <h1>About Me:</h1>
       <div className='about-flex-container'>
-        <img src="src/assets/images/Ryan.jpg" alt="man with blonde hair smiling" className='profile-photo' />
+        <img src="./assets/images/Ryan.jpg" alt="man with blonde hair smiling" className='profile-photo' />
         <p className='about-description'>I am a full-stack software engineer who attacks problems with a creative, entrepreneurial mindset. I strive to grow and improve rather than simply get the job done. I have experience creating music as a producer and mixing engineer, and I continually carry that creativity and critical thinking into my full-stack software development.</p>
       </div>
     </>
